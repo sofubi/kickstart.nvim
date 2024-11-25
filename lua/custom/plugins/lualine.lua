a396a65072cc5b5f194db30ec5512f7a547467ca
Thomas Lawton <thomas.lawton@ratmail.cc>
2024-11-25 00:51:14 -0500

    Updates

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -3,14 +3,6 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
-      tabline = {
-        lualine_a = { 'buffers' },
-        lualine_b = { 'filename' },
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = { 'tabs' },
-      },
       extensions = {
         'quickfix',
         'lazy',
--- lua/custom/plugins/norg.lua
+++ lua/custom/plugins/norg.lua
@@ -1,49 +1,50 @@
-return {
-  {
-    'nvim-neorg/neorg',
-    ft = 'norg',
-    cmd = 'Norg',
-    version = '*',
-    dependencies = {
-      { 'nvim-lua/plenary.nvim' },
-      { 'nvim-neorg/neorg-telescope' },
-      { 'vhyrro/luarocks.nvim' },
-    },
-    lazy = false,
-    config = function()
-      require('neorg').setup {
-        load = {
-          ['core.defaults'] = {},
-          ['core.concealer'] = {},
-          ['core.integrations.nvim-cmp'] = {},
-          ['core.export'] = {},
-          ['core.completion'] = { config = { engine = 'nvim-cmp', name = '[Norg]' } },
-          ['core.keybinds'] = {
-            config = {
-              default_keybinds = true,
-              neorg_leader = '<leader>N',
-            },
-          },
-          ['core.dirman'] = {
-            config = {
-              workspaces = {
-                notes = '~/Documents/Notes',
-              },
-              default_workspace = 'notes',
-            },
-          },
-          ['core.integrations.telescope'] = {},
-        },
-      }
-      vim.wo.foldlevel = 99
-      vim.wo.conceallevel = 2
-    end,
-    keys = {
-      {
-        '<leader>N',
-        '<Plug>(neorg.telescope.find_norg_files)',
-        desc = 'Open Neorg index',
-      },
-    },
-  },
-}
+return {}
+-- return {
+--   {
+--     'nvim-neorg/neorg',
+--     ft = 'norg',
+--     cmd = 'Norg',
+--     version = '*',
+--     dependencies = {
+--       { 'nvim-lua/plenary.nvim' },
+--       { 'nvim-neorg/neorg-telescope' },
+--       { 'vhyrro/luarocks.nvim' },
+--     },
+--     lazy = false,
+--     config = function()
+--       require('neorg').setup {
+--         load = {
+--           ['core.defaults'] = {},
+--           ['core.concealer'] = {},
+--           ['core.integrations.nvim-cmp'] = {},
+--           ['core.export'] = {},
+--           ['core.completion'] = { config = { engine = 'nvim-cmp', name = '[Norg]' } },
+--           ['core.keybinds'] = {
+--             config = {
+--               default_keybinds = true,
+--               neorg_leader = '<leader>N',
+--             },
+--           },
+--           ['core.dirman'] = {
+--             config = {
+--               workspaces = {
+--                 notes = '~/Documents/Notes',
+--               },
+--               default_workspace = 'notes',
+--             },
+--           },
+--           ['core.integrations.telescope'] = {},
+--         },
+--       }
+--       vim.wo.foldlevel = 99
+--       vim.wo.conceallevel = 2
+--     end,
+--     keys = {
+--       {
+--         '<leader>N',
+--         '<Plug>(neorg.telescope.find_norg_files)',
+--         desc = 'Open Neorg index',
+--       },
+--     },
+--   },
+-- }
