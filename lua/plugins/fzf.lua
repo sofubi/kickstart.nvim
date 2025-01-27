93a6e88fbbf4934d6f07f1fdfd8e58953bc3a890
thomaslawton-ngt <thomas.lawton@numerated.com>
2025-01-27 09:38:52 -0500

    Updates

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -7,8 +7,38 @@ return {
       { 'nvim-tree/nvim-web-devicons' },
     },
     config = function()
-      -- calling `setup` is optional for customization
-      require('fzf-lua').setup {
+      local fzf_lua = require 'fzf-lua'
+      local function open_scratch_buffer(filetype)
+        local date_time = os.date '%Y-%m-%d_%H-%M-%S'
+        local scratch_dir = vim.fn.expand '~/.local/share/nvim/scratches/'
+        local filename = scratch_dir .. date_time .. '.' .. filetype
+
+        vim.fn.mkdir(scratch_dir, 'p')
+        vim.cmd('vnew ' .. vim.fn.fnameescape(filename))
+        vim.api.nvim_set_option_value('filetype', filetype, {})
+        vim.api.nvim_command 'write'
+      end
+      function _G.select_filetype_and_create_scratch()
+        local syntax_dir = vim.fn.globpath(vim.fn.getenv 'VIMRUNTIME', 'syntax/*.vim', false, true)
+        local filetypes = {}
+        for _, filepath in ipairs(syntax_dir) do
+          local filetype = filepath:match 'syntax[/\\](.+).vim$'
+          if filetype then
+            table.insert(filetypes, filetype)
+          end
+        end
+
+        fzf_lua.fzf_exec(filetypes, {
+          prompt = 'Filetypes> ',
+          actions = {
+            ['default'] = function(selected)
+              open_scratch_buffer(selected[1])
+            end,
+          },
+        })
+      end
+
+      fzf_lua.setup {
         { 'max-perf', 'fzf-native' },
         'sk',
         winopts = {
@@ -20,6 +50,13 @@ return {
     end,
     keys = {
       -- buffers / files
+      {
+        '<C-s>',
+        function()
+          _G.select_filetype_and_create_scratch()
+        end,
+        desc = 'Create scratch with ft',
+      },
       {
         '<leader>,',
         function()
